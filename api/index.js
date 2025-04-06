c1adc169746fb62f35fa601be163e00391255241
oloukpede prosper <oloukpedeprosper@gmail.com>
2025-04-06 17:42:34 +0100

    deuxieme commit

--- api/index.js
+++ api/index.js
@@ -6,10 +6,10 @@ const app = express();
 
 // Définir le moteur de rendu pour les fichiers EJS
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "../src/views/pages"));
+app.set("views", path.join(process.cwd(), "src/views/pages"));
 
 // Servir les fichiers statiques
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(process.cwd(), "public"))); // Utiliser process.cwd()
 
 // Routes
 app.get("/", (req, res) => {
@@ -21,11 +21,17 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/nouvelle_commande", (req, res) => {
+  // Calculer les chemins absolus à transmettre à la vue
+  const headerPath = path.join(process.cwd(), 'src/views/components/header');
+  const menuPath = path.join(process.cwd(), 'src/views/components/menu');
+
   res.render("serveur/nouvelle_commande", {
     utilisateur: {
       nom: "Paul BOAKE",
       role: "serveur"
-    }
+    },
+    headerPath: headerPath,
+    menuPath: menuPath
   });
 });
 
